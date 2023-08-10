85ddf105ebf8672e2ba135a98e9c5f48a9c5a259
Hannah Schwaeble <hannah.schwaeble@gmail.com>
2023-08-10 16:07:11 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout (string) {
+    return string.toUpperCase();
+}
+function whisper (string) {
+    return string.toLowerCase();
+}
+function logShout (string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper (string) {
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate (string) {  
+    const cantHear = "I can\'t hear you!"
+    const yesAnswer = "YES INDEED!"
+    const dinnerAnswer = "I would love to!"
+if (string.toLowerCase(string) === string) { 
+    return cantHear
+}
+    if (string.toUpperCase(string) === string) {
+        return yesAnswer
+}
+else if (("Let\'s have dinner together!") === string) {
+    return dinnerAnswer
+}
+}
+
